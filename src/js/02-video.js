a8aa25674aa16605e30984591d0154f421e2ef78
1Kostik <117483719+1Kostik@users.noreply.github.com>
2023-03-30 11:56:28 +0300

    up_task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,12 +5,14 @@ const iframe = document.querySelector('iframe');
 
 const player = new Player(iframe);
 
-player.on('timeupdate', throttle(function (data) {
-  localStorage.setItem('videoplayer-current-time', `${data.seconds}`)
-}, 1000));
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', `${data.seconds}`);
+  }, 1000)
+);
 
 const seconds = localStorage.getItem('videoplayer-current-time');
-console.log(error);
 if (seconds) {
   player.setCurrentTime(seconds).catch(function (error) {
     console.log(error);
